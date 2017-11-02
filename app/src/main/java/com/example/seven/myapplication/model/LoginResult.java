48ae9b238c995bb5ce5f0a9f07cda63e299a863e
daichen <daichen@kbfoo.com>
2017-11-02 20:22:40 +0800

    添加退款接口

--- app/src/main/java/com/example/seven/myapplication/model/LoginResult.java
+++ app/src/main/java/com/example/seven/myapplication/model/LoginResult.java
@@ -25,3 +25,4 @@ public class LoginResult {
         this.result = result;
     }
 }
+
---
+++ app/src/main/java/com/example/seven/myapplication/service/RefundService.java
@@ -0,0 +1,20 @@
+package com.example.seven.myapplication.service;
+
+import com.example.seven.myapplication.model.RefundResult;
+import com.example.seven.myapplication.network.CommonCallback;
+
+/**
+ * Created by daichen on 2017/11/2.
+ */
+
+public class RefundService {
+    public void refund(String amount,String  payCode,CommonCallback commonCallback){
+
+    }
+
+    public RefundResult getRefundResult(String data){
+        RefundResult refundResult = new RefundResult();
+
+        return refundResult;
+    }
+}
